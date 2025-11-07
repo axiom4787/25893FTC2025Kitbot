e58f259b5e5225e68bab990fbb627be7a09b5cd5
hheehee <hhalloran@cps.edu>
2025-11-07 15:19:00 -0600

    Add basic move forward auto

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicAuto_MoveForward.java
@@ -0,0 +1,68 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+@Autonomous(name="Basic autonomous move forward", group="Basic Auto")
+public class BasicAuto_MoveForward extends LinearOpMode {
+
+    DcMotor frontLeftDrive, backLeftDrive, frontRightDrive, backRightDrive;
+    ElapsedTime runtime = new ElapsedTime();
+
+    @Override
+    public void runOpMode() {
+        frontLeftDrive = hardwareMap.get(DcMotor.class, "leftFront");
+        backLeftDrive = hardwareMap.get(DcMotor.class, "leftBack");
+        frontRightDrive = hardwareMap.get(DcMotor.class, "rightFront");
+        backRightDrive = hardwareMap.get(DcMotor.class, "rightBack");
+
+        frontLeftDrive.setDirection(DcMotor.Direction.FORWARD);
+        backLeftDrive.setDirection(DcMotor.Direction.FORWARD);
+        frontRightDrive.setDirection(DcMotor.Direction.REVERSE);
+        backRightDrive.setDirection(DcMotor.Direction.REVERSE);
+
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        waitForStart();
+        runtime.reset();
+
+        telemetry.addData("Status", "Running");
+        telemetry.update();
+
+        move(0.5);
+
+        sleep(500);
+
+        stopRobot();
+
+        telemetry.addData("Status", "Done/stopped");
+        telemetry.update();
+    }
+    void sleep(int ms) {
+        double startTime = runtime.milliseconds();
+        while(opModeIsActive() && (runtime.milliseconds() - startTime) < ms);
+        // Wait for `ms` milliseconds
+    }
+
+    void move(double speed) {
+        if(!opModeIsActive()) {
+            stopRobot();
+            return;
+        } // Stop the robot if it shouldn't me moving
+
+        frontLeftDrive.setPower(speed);
+        frontRightDrive.setPower(speed);
+        backLeftDrive.setPower(speed);
+        backRightDrive.setPower(speed);
+    }
+
+    void stopRobot() {
+        frontLeftDrive.setPower(0);
+        frontRightDrive.setPower(0);
+        backLeftDrive.setPower(0);
+        backRightDrive.setPower(0);
+    }
+}
